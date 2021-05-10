dc6ee8a9acfdbbb211616c03f2664cf113a65516
BenjiCoder02 <benkoshy02@gmail.com>
2021-05-10 18:35:05 -0400

    Changes to conditionals and UI

--- app.js
+++ app.js
@@ -24,6 +24,41 @@ function time(){
     hours = d.getHours();
     minutes = d.getMinutes();
     seconds = d.getSeconds();
+
+    if(hours < 7) {
+        body.style.backgroundImage = "url('https://images.pexels.com/photos/1335115/pexels-photo-1335115.jpeg?cs=srgb&dl=pexels-konevi-1335115.jpg&fm=jpg')"
+        body.style.height= "100vh";
+         body.style.backgroundSize = "cover";
+         body.style.backgroundRepeat = "no-repeat"
+         body.style.transition = "background-image 0.4s ease-in-out";
+   }
+
+ else if (hours > 7 && hours < 15){
+    
+        body.style.backgroundImage = "url('https://images.pexels.com/photos/1680140/pexels-photo-1680140.jpeg?cs=srgb&dl=pexels-daria-shevtsova-1680140.jpg&fm=jpg')"
+        body.style.backgroundSize = "cover";
+         body.style.height= "100vh";
+         body.style.backgroundRepeat = "no-repeat"
+         body.style.transition = "background-image 0.4s ease-in-out";
+   }
+
+   else if (hours >= 15 && hours <= 18){
+     
+        body.style.backgroundImage = "url('https://images.pexels.com/photos/3355788/pexels-photo-3355788.jpeg?cs=srgb&dl=pexels-quang-nguyen-vinh-3355788.jpg&fm=jpg')"
+        body.style.backgroundSize = "cover";
+         body.style.height= "100vh";
+         body.style.backgroundRepeat = "no-repeat"
+         body.style.transition = "background-image 0.4s ease-in-out";
+   }
+
+   else if (hours > 18) {
+      body.style.backgroundImage = "url('https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?cs=srgb&dl=pexels-vlad-bagacian-1368382.jpg&fm=jpg')"
+        body.style.backgroundSize = "cover";
+         body.style.backgroundRepeat = "no-repeat"
+         body.style.transition = "background-image 0.4s ease-in-out";
+   }
+
+
     if (hours > 11){
         ap = "PM";
     }
@@ -34,14 +69,14 @@ function time(){
     if(seconds < 10){
        seconds = "0" + seconds;
     }
-    if(hours > 12){
-        hours = hours - 12;
-    }
+   
     if(hours === 0){
         hours = 12;
     }
     var t = document.querySelector(".clock").innerHTML = `${hours} : ${minutes} ${ap} `;
-//Date Display
+
+
+    //Date Display
 
     var date = d.getDate();
    
@@ -56,34 +91,7 @@ function time(){
 
 
     //Img change based on time of day
-    if(hours < 7 && ap === "AM") {
-         body.style.backgroundImage = "url('https://images.pexels.com/photos/1335115/pexels-photo-1335115.jpeg?cs=srgb&dl=pexels-konevi-1335115.jpg&fm=jpg')"
-         body.style.height= "100vh";
-          body.style.backgroundSize = "cover";
-          body.style.backgroundRepeat = "no-repeat"
-    }
-
-  else if (hours > 7 && hours < 3){
-     
-         body.style.backgroundImage = "url('https://images.pexels.com/photos/1680140/pexels-photo-1680140.jpeg?cs=srgb&dl=pexels-daria-shevtsova-1680140.jpg&fm=jpg')"
-         body.style.backgroundSize = "cover";
-          body.style.height= "100vh";
-          body.style.backgroundRepeat = "no-repeat"
-    }
-
-    else if (hours > 3 && ap === "PM"){
-      
-         body.style.backgroundImage = "url('https://images.pexels.com/photos/3355788/pexels-photo-3355788.jpeg?cs=srgb&dl=pexels-quang-nguyen-vinh-3355788.jpg&fm=jpg')"
-         body.style.backgroundSize = "cover";
-          body.style.height= "100vh";
-          body.style.backgroundRepeat = "no-repeat"
-    }
-
-    else if (hours > 6 && ap === "PM") {
-       body.style.backgroundImage = "url('https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?cs=srgb&dl=pexels-vlad-bagacian-1368382.jpg&fm=jpg')"
-         body.style.backgroundSize = "cover";
-          body.style.backgroundRepeat = "no-repeat"
-    }
+   
    
   
   
