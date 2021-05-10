31a5c673f870f6cf5558f83c8ac75f765f473ce8
BenjiCoder02 <benkoshy02@gmail.com>
2021-05-10 17:45:40 -0400

    completed clock app

---
+++ app.js
@@ -0,0 +1,93 @@
+function time(){
+    var d = new Date();
+    var body = document.querySelector("body");
+
+ 
+
+//Days and Months Stored in Arrays
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var month = [
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ]
+
+    //Time Display
+
+    ap = "AM";
+    hours = d.getHours();
+    minutes = d.getMinutes();
+    seconds = d.getSeconds();
+    if (hours > 11){
+        ap = "PM";
+    }
+    if(minutes < 10  ){
+        minutes = "0" + minutes;
+       
+    }
+    if(seconds < 10){
+       seconds = "0" + seconds;
+    }
+    if(hours > 12){
+        hours = hours - 12;
+    }
+    if(hours === 0){
+        hours = 12;
+    }
+    var t = document.querySelector(".clock").innerHTML = `${hours} : ${minutes} ${ap} `;
+//Date Display
+
+    var date = d.getDate();
+   
+    var year = d.getFullYear();
+
+    var day = weekday[d.getDay()];
+    mon = month[d.getMonth()]
+
+    var DD = document.querySelector(".date");
+
+    DD.innerHTML = `${day},  ${mon} ${date},   ${year}`
+
+
+    //Img change based on time of day
+    if(hours < 7 && ap === "AM") {
+         body.style.backgroundImage = "url('https://images.pexels.com/photos/1335115/pexels-photo-1335115.jpeg?cs=srgb&dl=pexels-konevi-1335115.jpg&fm=jpg')"
+         body.style.height= "100vh";
+          body.style.backgroundSize = "cover";
+          body.style.backgroundRepeat = "no-repeat"
+    }
+
+  else if (hours > 7 && hours < 3){
+     
+         body.style.backgroundImage = "url('https://images.pexels.com/photos/1680140/pexels-photo-1680140.jpeg?cs=srgb&dl=pexels-daria-shevtsova-1680140.jpg&fm=jpg')"
+         body.style.backgroundSize = "cover";
+          body.style.height= "100vh";
+          body.style.backgroundRepeat = "no-repeat"
+    }
+
+    else if (hours > 3 && ap === "PM"){
+      
+         body.style.backgroundImage = "url('https://images.pexels.com/photos/3355788/pexels-photo-3355788.jpeg?cs=srgb&dl=pexels-quang-nguyen-vinh-3355788.jpg&fm=jpg')"
+         body.style.backgroundSize = "cover";
+          body.style.height= "100vh";
+          body.style.backgroundRepeat = "no-repeat"
+    }
+
+    else if (hours > 6 && ap === "PM") {
+       body.style.backgroundImage = "url('https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?cs=srgb&dl=pexels-vlad-bagacian-1368382.jpg&fm=jpg')"
+         body.style.backgroundSize = "cover";
+          body.style.backgroundRepeat = "no-repeat"
+    }
+   
+  
+  
+
+    t = setTimeout("time()", 500)
+}
+
